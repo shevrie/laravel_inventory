61b353a01fcb7b0560d91ef4592bfc9e62aa90ed
Shevrie Maulana <shevrie18@gmail.com>
2020-07-09 22:12:10 +0700

    Create Model and Controller Products

---
+++ database/migrations/2020_07_07_083626_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->string('product_id', 25)->primary();
+            $table->string('product_name', 255);
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
